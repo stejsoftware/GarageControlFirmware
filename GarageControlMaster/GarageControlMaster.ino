1311f14c65c450a013f043e4752d267330f942a8
Jonathan Meyer <jon@stejsoftware.com>
2016-03-13 18:40:54 -0400

    updated master sketch

--- GarageControlMaster/GarageControlMaster.ino
+++ GarageControlMaster/GarageControlMaster.ino
@@ -1,256 +1,321 @@
-#include <Wire.h>
-#include <Adafruit_LEDBackpack.h>
-#include <Adafruit_GFX.h>
-#include <CmdMessenger.h>
-#include <GarageControl.h>
-#include <Button.h>
-#include <LED.h>
-#include <Timer.h>
-
-#include<stdlib.h>
+#include "Wire.h"
+#include "Adafruit_LEDBackpack.h"
+#include "Adafruit_GFX.h"
+#include "CmdMessenger.h"
+#include "GarageControl.h"
+#include "Led_Matrix.h"
+#include "Button.h"
+#include "Led.h"
+#include "Timer.h"
 
 enum MODE
 {
-  ShowTemp,
-  ShowHumid,
-  ShowDoor,
-  LAST
+  RUN,
+  SETUP,
+  BRIGHT,
+  TEMP,
+  SPEED,
 };
 
 Button openButton(2);
 Button closeButton(3);
-
-LED openLed(5);
-LED closeLed(6);
-
-Timer LedTimer;
-
 Button modeButton(7);
-MODE mode = ShowTemp;
+
+Led_Matrix screen(16, 8, 2, 1);
+
+Led openLed(5);
+Led closeLed(6);
 
 CmdMessenger cmd(Serial);
 
-#define MATRIX_COUNT 2
+float temperature = 0.0;
+float humidity = 0.0;
+DS door_status = DS_Unknown;
+MODE mode = RUN;
+MODE submode = BRIGHT;
+Alarm * watchdog = NULL;
 
-Adafruit_8x8matrix matrix[] = {
-  Adafruit_8x8matrix(),
-  Adafruit_8x8matrix()
-};
+// settings
+int8_t brightness = 50;
+bool celsius = false;
+bool fast = true;
 
-Timer d_timer;
-String d_text;
-int8_t d_length = 0;
-int8_t d_cursor = 0;
-bool d_sleft = true;
+void update_display()
+{
+  screen.clearText();
+  screen.setBrightness((brightness / 100.0) * 15);
+  
+  closeLed.setMax(brightness);
+  openLed.setMax(brightness);
+  
+  switch( mode )
+  {
+    case RUN:
+      {
+      bool moving_door = false;
+
+      switch( door_status )
+      {
+        case DS_Open:
+          closeLed.off();
+          openLed.on();
+        break;
+
+        case DS_Closed:
+          openLed.off();
+          closeLed.on();
+        break;
+
+        case DS_Opening:
+          case DS_Closing:
+          openLed.off();
+          closeLed.off();
+          moving_door = true;
+        break;
+        
+        default:
+          ; // do nothing
+      }
+
+      if (moving_door)
+      {
+        screen << F("** ") << toString(door_status);
+      }
+      else
+      {
+        screen << (int16_t) round(temperature)
+            << char(0xf7) << F(" ") // a degree symbol
+            << (int16_t) round(humidity) << F("% ")
+            << toString(door_status);
+      }
+    }
+    break;
+
+    case SETUP:
+    {
+      screen.resetFrame();
+      openLed.on();
+      closeLed.on();
+      
+      if( watchdog != NULL )
+      {
+        watchdog->reset();
+      }
+      
+      switch( submode )
+      {
+        case BRIGHT:
+          screen << brightness << "%" << F("<<Brightness");
+        break;
+
+        case TEMP:
+          screen << (celsius ? "C" : "F") << F("<<Temp Scale");
+        break;
+
+        case SPEED:
+          screen << (fast ? "Fast" : "Slow") << F("<<Scroll Speed");
+        break;
+        
+        default:
+          ; // do nothing
+      }
+    }
+    break;
+    
+    default:
+      ; // do nothing
+  }
+}
 
 void OnTemperature()
 {
-  float value = 0.0;
-  char buff[32] = {0};
-  char suffix[2] = {0};
-
-  if( mode == ShowTemp )
-  {
-    value = cmd.readFloatArg();
-    suffix[0] = char(0xf7);
-  }
-  else
-  if( mode == ShowHumid )
-  {
-    value = cmd.readFloatArg();
-    value = cmd.readFloatArg();
-    suffix[0] = '%';
-  }
-
-  if( value == int(value) )
-  {
-    itoa(int(value), buff, 10);
-  }
-  else
-  {
-    dtostrf(value, 4, 1, buff);
-  }
-
-  set_text(strcat(buff, suffix));
-  cmd.sendCmd(GC_Acknowledge, "temp");
+  temperature = cmd.readFloatArg();
+  humidity = cmd.readFloatArg();
+  update_display();
+  cmd.sendCmd(GC_Acknowledge, F("temp"));
 }
 
 void OnDoorStatus()
 {
-  DS door = (DS)cmd.readIntArg();
-  String s_door = toString(door);
-  
-  if( mode == ShowDoor )
+  door_status = (DS) cmd.readInt32Arg();
+  update_display();
+  cmd.sendCmd(GC_Acknowledge, F("door"));
+}
+
+void get_status()
+{
+  screen.clearText();
+  screen << F("**");
+  cmd.sendCmd(GC_GetTemperature, F("temp?"));
+  cmd.sendCmd(GC_GetDoorStatus, F("door?"));
+}
+
+void change_value(int8_t inc)
+{
+  switch( submode )
   {
-    switch( door )
+    case BRIGHT:
+      brightness += (inc * 10);
+
+      if( brightness < 0 )
+      {
+        brightness = 0;
+      }    
+      
+      if( brightness > 100 )
+      {
+        brightness = 100;
+      }    
+    break;
+    
+    case TEMP:
+      celsius = !celsius;
+    break;
+    
+    case SPEED:
+      fast = !fast;
+    break;
+    
+    default:
+      ; // do nothing
+  }
+  
+  update_display();
+}
+
+void open_pressed(Button & button)
+{
+  if (mode == RUN)
+  {
+    cmd.sendCmd(GC_OpenDoor, F("Open the door!"));
+  }
+  else
+  {
+    change_value(-1);
+  }
+}
+
+void close_pressed(Button & button)
+{
+  if (mode == RUN)
+  {
+    cmd.sendCmd(GC_CloseDoor, F("Close the door!"));
+  }
+  else
+  {
+    change_value(+1);
+  }
+}
+
+void mode_click(Button & button)
+{
+  if (mode == RUN)
+  {
+    get_status();
+  }
+  else
+  {
+    switch( submode )
     {
-      case DS_Open:
-        set_text("Open");
+      case BRIGHT:
+        submode = TEMP;
       break;
 
-      case DS_Closed:
-        set_text("Closed");
+      case TEMP:
+        submode = SPEED;
+      break;
+
+      case SPEED:
+        submode = BRIGHT;
       break;
       
       default:
-        set_text("");
-    }
-  }
+        ; // do nothing
+   }
 
-  if( door == DS_Open )
-  {
-    openLed.on();
-  }
-  else
-  {
-    openLed.brightness(10);
-  }
-
-  if( door == DS_Closed )
-  {
-    closeLed.on();
-  }
-  else
-  { 
-    closeLed.brightness(10);
-  }
-
-  cmd.sendCmd(GC_Acknowledge, "door");
-}
-
-void set_text(const String & text)
-{
-  if( text != d_text )
-  {
-    d_text = text;
-    d_length = (d_text.length() * 6) - 16;
-    d_cursor = 0;
-    d_timer.start(1);
-//    cmd.sendCmd(GC_Acknowledge, d_length);
+    update_display();
   }
 }
 
-void show_next_frame()
+void mode_held(Button & button)
 {
-  for( uint8_t m = 0; m < MATRIX_COUNT; m++ )
+  switch( mode )
   {
-    matrix[m].clear();
-   
-    if( d_text.length() > 0 )
-    {
-      matrix[m].setTextSize(1);
-      matrix[m].setTextWrap(false);  // we dont want text to wrap so it scrolls nicely
-      matrix[m].setTextColor(LED_ON);
-      matrix[m].setCursor(d_cursor - (8 * m), 0);
-      matrix[m].print(d_text);
-    }
+    case RUN:
+      mode = SETUP;
+      
+      if( watchdog == NULL )
+      {
+        watchdog = Timer.delay(exit_setup, 30000);
+      }      
+
+      update_display();
+    break;
+
+    case SETUP:
+      exit_setup();
+    break;
     
-    matrix[m].writeDisplay();
+    default:
+      ; // do nothing
   }
+}
+
+void exit_setup()
+{
+  mode = RUN;
   
-  if( d_length > 0 )
+  if( watchdog != NULL )
   {
-    if( d_cursor <= -d_length )
-    {  
-      d_sleft = false;
-      d_timer.start(3000);
-    }
-    else
-    if( d_cursor == 0 )
-    {
-      d_sleft = true;
-      d_timer.start(3000);
-    }
-    else
-    {
-      d_timer.start(100);
-    }
-    
-    d_cursor = (d_sleft ? d_cursor - 1 : d_cursor + 1);
-  }
+    watchdog->stop();
+    watchdog = NULL;
+  }  
+  
+  update_display();
 }
 
 // Setup function
-void setup() 
-{  
-  Serial.begin(9600); 
-  Wire.begin();
-  
+void setup()
+{
+  Serial.begin(9600);
+
+  openButton.attach(bePressed, open_pressed);
+  closeButton.attach(bePressed, close_pressed);
+  modeButton.attach(beClick, mode_click);
+  modeButton.attach(beHeld, mode_held);
+
+  screen.begin();
+
   // Adds newline to every command
-  cmd.printLfCr();   
-  
-  cmd.attach(GC_DoorStatus,  &OnDoorStatus);
+  cmd.printLfCr();
+
+  cmd.attach(GC_DoorStatus, &OnDoorStatus);
   cmd.attach(GC_Temperature, &OnTemperature);
- 
+
   cmd.sendCmd(GC_Acknowledge, F("Garage Control Master Started!"));
 
-  for( uint8_t i = 0; i < MATRIX_COUNT; i++ ) 
-  {
-    matrix[i].begin(0x70 + i);
-    matrix[i].clear();
-    matrix[i].writeDisplay();
-  }
-    
-  cmd.sendCmd(GC_GetTemperature, "temp?");
-  cmd.sendCmd(GC_GetDoorStatus, "door?");
-  
-  // test indicators
-  openLed.on();
-  closeLed.on();  
-  set_text("***");
-  show_next_frame();
-  
-  delay(1000);
-
-  closeLed.off();
-  openLed.off();  
-  set_text("");
-  show_next_frame();
-
-  d_timer.start(1);
-  LedTimer.interval(30);
+  get_status();
 }
 
 // Loop function
-void loop() 
+void loop()
 {
   // process serial commands
   cmd.feedinSerialData();
-  
-  if( openButton.closed() )
-  {
-    cmd.sendCmd(GC_OpenDoor, "Open the door!");
-    openLed.flash();
-  }
-  
-  if( closeButton.closed() )
-  {
-    cmd.sendCmd(GC_CloseDoor, "Close the door!");
-    closeLed.flash();
-  }
 
-  if( modeButton.closed() )
-  {
-    cmd.sendCmd(GC_GetTemperature, "temp?");
-    cmd.sendCmd(GC_GetDoorStatus, "door?");
-    
-    mode = (MODE)(mode + 1);
-    
-    if( mode == LAST )
-    {
-      mode = (MODE)0;
-    }
-  }
+  // read buttons
+  openButton.run();
+  closeButton.run();
+  modeButton.run();
 
-  if( d_timer.isTime() )
-  {
-    show_next_frame();
-  }
+  // update led matrix
+  screen.run(mpScrollLeft, (fast ? 100 : 200));
+
+  // update leds
+  closeLed.display();
+  openLed.display();
   
-  if( LedTimer.isTime() )
-  {
-    closeLed.display();
-    openLed.display();
-  }
+  // timer
+  Timer.run();
 }
 
