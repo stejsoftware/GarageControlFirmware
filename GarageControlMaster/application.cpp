280c2675a56f9cc7885a3781fd134ab8eff8b734
Jonathan Meyer <jon@stejsoftware.com>
2016-08-21 17:21:27 -0400

    can open and close from blynk or particle cloud.

--- GarageControlMaster/application.cpp
+++ GarageControlMaster/application.cpp
@@ -8,21 +8,14 @@
 #include "ArduinoLibrary/Led.h"
 #include "ArduinoLibrary/GarageControl.h"
 #include "blynk/blynk.h"
-#include "Config.h"
 
 // time defines
 #define SECOND_MS 1000
 #define MINUTE_MS 60000
 #define HOUR_MS   3600000
 
-enum MODE
-{
-  RUN,
-  SETUP,
-  BRIGHT,
-  TEMP,
-  SPEED,
-};
+// button functions
+void onButtonPressed(Button & button);
 
 // cloud functions
 int funcDoor(String command);
@@ -30,19 +23,26 @@ int funcDoor(String command);
 // timer functions
 void onFiveMinutes();
 void onFiveSeconds();
+void onThirtySeconds();
+
+// other functions
+void lcdPrint(int line, const String & text);
+void debug(const String & text);
+void openDoor();
+void closeDoor();
 
 // global things
 Button openButton(4);
 Button closeButton(5);
 Led_Matrix screen(16, 8, 2, 1, 256);
-WidgetLCD lcd(V2);
-WidgetTerminal terminal(V3);
 Led openLed(2);
 Led closeLed(3);
 CmdMessenger cmd(Serial1);
-Config config;
 Timer fiveMinuteTimer(MINUTE_MS * 5, onFiveMinutes);
 Timer fiveSecondTimer(SECOND_MS * 5, onFiveSeconds);
+Timer thirtySecondTimer(SECOND_MS * 30, onThirtySeconds);
+WidgetLCD lcd(V2);
+WidgetTerminal terminal(V3);
 
 // global vars
 int rssi = 0;
@@ -54,131 +54,93 @@ DS door_status = DS_Unknown;
 
   // settings
 int16_t brightness = 100;
-bool celsius = false;
-bool fast = true;
 
 SYSTEM_THREAD(ENABLED);
 
-void update_display()
+void updateDisplay()
 {
   screen.clearText();
-  lcd.clear();
-  screen.setBrightness((brightness / 100.0) * 15);
 
-  closeLed.setMax(brightness);
-  openLed.setMax(brightness);
+  screen
+    << (int16_t) round(temperature)
+    << char(0xf7) << F(" ") // a degree symbol
+    //<< (int16_t) round(humidity) << F("% ")
+    << toString(door_status)
+  ;
+}
 
-  bool moving_door = false;
+void onTemperature()
+{
+  //debug("onTemperature()");
+
+  temperature = cmd.readFloatArg();
+  humidity = cmd.readFloatArg();
+
+  Blynk.virtualWrite(V6, temperature);
+  Blynk.virtualWrite(V7, humidity);
+  
+  cmd.sendCmd(GC_Acknowledge, F("temp"));
+  updateDisplay();
+}
+
+void lcdPrint(int line, const String & text)
+{
+  String _text = text;
+  
+  while( _text.length() < 16 )
+  {
+    _text += F(" ");
+  }
+  
+  lcd.print(0, line, _text);
+}
+
+void onDoorStatus()
+{
+  //debug("onDoorStatus()");
+
+  door_status = (DS) cmd.readInt32Arg();
+
+  lcdPrint(0, toString(door_status));
+  
+  openLed.off();
+  closeLed.off();
 
   switch( door_status )
   {
     case DS_Open:
+      openLed.on();
+      break;
+    
     case DS_Closed:
-      openLed.off();
-      closeLed.off();
-    break;
-
-    case DS_Opening:
-      if( !openLed.isRunning() )
-      {
-        openLed.flash();
-      }
-
-      moving_door = true;
-    break;
-
-    case DS_Closing:
-      if( !closeLed.isRunning() )
-      {
-        closeLed.flash();
-      }
-
-      moving_door = true;
-    break;
-
-    default:
-      ; // do nothing
+      closeLed.on();
+      break;
   }
-
-  if (moving_door)
-  {
-    screen << F("** ") << toString(door_status);
-  }
-  else
-  {
-    screen
-      << (int16_t) round(temperature)
-      << char(0xf7) << F(" ") // a degree symbol
-      //<< (int16_t) round(humidity) << F("% ")
-      << toString(door_status)
-      //<< F("Tabitha & Zibby! I don't know the true status of the door casue the sensors are broken! :P ")
-      ;
-      
-    lcd.print(0, 0, toString(door_status));
-    lcd.print(0, 1, String::format("%d%c %d%%",(int16_t) round(temperature), char(0xf7), (int16_t) round(humidity)));
-  }
-}
-
-void OnTemperature()
-{
-  terminal.print(Time.format(Time.now(), "%H:%M:%S "));
-  terminal.println("OnTemperature()");
-  terminal.flush();
   
-  temperature = cmd.readFloatArg();
-  humidity = cmd.readFloatArg();
-
-  update_display();
-
-  cmd.sendCmd(GC_Acknowledge, F("temp"));
-}
-
-void OnDoorStatus()
-{
-  terminal.print(Time.format(Time.now(), "%H:%M:%S "));
-  terminal.println("OnDoorStatus()");
-  terminal.flush();
-
-  door_status = (DS) cmd.readInt32Arg();
-
-  update_display();
-
   cmd.sendCmd(GC_Acknowledge, F("door"));
+  updateDisplay();
 }
 
-void get_status()
+void onOpenPressed(Button & button)
 {
-  screen.clearText();
-  screen << F("Querying the Garage");
-
-  cmd.sendCmd(GC_GetTemperature, F("temp?"));
-  cmd.sendCmd(GC_GetDoorStatus, F("door?"));
+  openDoor();
 }
 
-void open_pressed(Button & button)
+void onClosePressed(Button & button)
 {
-  openLed.flash();
-  cmd.sendCmd(GC_OpenDoor, F("Open the door!"));
-  Particle.publish("Button", "Open", PRIVATE);
-}
-
-void close_pressed(Button & button)
-{
-  closeLed.flash();
-  cmd.sendCmd(GC_CloseDoor, F("Close the door!"));
-  Particle.publish("Button", "Close", PRIVATE);
+  closeDoor();
 }
 
 void onFiveMinutes()
 {
   if( send_temp )
   {
-    Particle.publish("Temp", String::format("%f", temperature), PRIVATE);
+    Particle.publish(F("Temp"), String::format(F("%f"), temperature), PRIVATE);
     send_temp = false;
   }
   else
   {
-    Particle.publish("Humidity", String::format("%f", humidity), PRIVATE);
+    Particle.publish(F("Humidity"), String::format(F("%f"), humidity), PRIVATE);
     send_temp = true;
   }
 }
@@ -186,6 +148,12 @@ void onFiveMinutes()
 void onFiveSeconds()
 {
   rssi = WiFi.RSSI();
+  Blynk.virtualWrite(V8, rssi);
+}
+
+void onThirtySeconds()
+{
+  cmd.sendCmd(GC_GetDoorStatus, F("door?"));
 }
 
 int funcDoor(String command)
@@ -199,69 +167,108 @@ int funcDoor(String command)
   else
   if( strcmp(command, "Open") == 0 )
   {
-    //cmd.sendCmd(GC_OpenDoor, F("Open the door!"));
+    openDoor();
     status = 3;
   }
   else
   if( strcmp(command, "Close") == 0 )
   {
-    cmd.sendCmd(GC_CloseDoor, F("Close the door!"));
+    closeDoor();
     status = 4;
   }
 
   return status;
 }
 
+void openDoor()
+{
+  debug("Open Door!");
+  openLed.flash();
+  cmd.sendCmd(GC_OpenDoor, F("Open the door!"));
+  Particle.publish("Button", "Open");
+}
+
+void closeDoor()
+{
+  debug("Close Door!");
+  closeLed.flash();
+  cmd.sendCmd(GC_CloseDoor, F("Close the door!"));  
+  Particle.publish("Button", "Close");
+}
+
+BLYNK_WRITE(V0) {
+  if (param.asInt() == 1) { // On button down...
+    openDoor();
+  }
+}
+
 BLYNK_WRITE(V1) {
   if (param.asInt() == 1) { // On button down...
-    close_pressed(closeButton);
+    closeDoor();
   }
+}
+
+void debug(const String & text)
+{
+  terminal.print(Time.format(Time.now(), "%H:%M:%S "));
+  terminal.println(text);
+  terminal.flush();
 }
 
 // Setup function
 void setup()
 {
+  // connection to the salve computer in the garage
   Serial1.begin(9600);
-  Serial1.print("ID: ");
-  Serial1.println(System.deviceID());
 
+  // register Particle variables
+  Particle.variable("rssi", rssi);
+  Particle.variable("temperature", temperature);
+  Particle.variable("humidity", humidity);
+
+  // register Particle functions
+  Particle.function("Door", funcDoor);
+
+  // connect to the blynk server
   Blynk.begin("7cb12eb10dee49f98c61d3e7b34f7433");
-  
-  config.subscribe();
-  config.load();
 
-  openButton.attach(bePressed, open_pressed);
-  closeButton.attach(bePressed, close_pressed);
+  while (Blynk.connect() == false) {
+    // Wait until connected
+  }
+
+  debug("STEJ Garage Control");
+  debug(String::format("Built: %s %s",__DATE__, __TIME__));
+  
+  closeLed.setMax(brightness);
+  openLed.setMax(brightness);
+
+  openButton.attach(bePressed, onOpenPressed);
+  closeButton.attach(bePressed, onClosePressed);
 
   screen.begin(0x73, true);
+  screen.setBrightness((brightness / 100.0) * 15);
+  screen.clearText();
+  
+  screen << "Now is the time for a very long message to see if it works!";
+  
+  lcd.clear();
 
   // Adds newline to every command
   cmd.printLfCr();
 
-  cmd.attach(GC_DoorStatus, &OnDoorStatus);
-  cmd.attach(GC_Temperature, &OnTemperature);
+  cmd.attach(GC_DoorStatus, &onDoorStatus);
+  cmd.attach(GC_Temperature, &onTemperature);
 
   cmd.sendCmd(GC_Acknowledge, F("Garage Control Master Started!"));
 
-  Particle.variable("rssi", rssi);
-  Particle.variable("temperature", temperature);
-  Particle.variable("humidity", humidity);
-  
-  Particle.function("Door", funcDoor);
-
+  onFiveSeconds(); fiveSecondTimer.start();
   fiveMinuteTimer.start();
-  fiveSecondTimer.start();
+  onThirtySeconds(); thirtySecondTimer.start();
 }
 
 // Loop function
 void loop()
 {
-  if( (door_status == DS_Unknown) && (Time.now() > last + 30) )
-  {
-    get_status();
-    last = Time.now();
-  }
-
   // process serial commands
   cmd.feedinSerialData();
 
@@ -270,11 +277,12 @@ void loop()
   closeButton.run();
 
   // update led matrix
-  screen.run(mpScrollLeft, (fast ? 100 : 200));
+  screen.run(mpScrollLeft, 100);
 
   // update leds
   closeLed.display();
   openLed.display();
-  
+
+  // update blynk
   Blynk.run();
 }
